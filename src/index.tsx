121cd8997d5afc9ded50f75f1cb9b4a7b903fd7f
Wataru Ono <wtr0414@gmail.com>
2021-10-19 23:26:43 +0900

    refactor: ルーティング周りの別コンポーネントへの切り分け
    可読性を上げるため

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import TopPage from '@/pages';
-import DetailPage from '@/pages/detail';
-import LoginPage from '@/pages/login';
-import ReviewPage from '@/pages/review';
+import Router from '@/components/Router';
 
 ReactDOM.render(
   <ChakraProvider>
     <React.StrictMode>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={TopPage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/pull-requests/:id" component={DetailPage} />
-          <Route exact path="/pull-requests/:id/review" component={ReviewPage} />
-        </Switch>
-      </BrowserRouter>
+      <Router />
     </React.StrictMode>
   </ChakraProvider>,
   document.getElementById('root')
