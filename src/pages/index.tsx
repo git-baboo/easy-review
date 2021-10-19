fa21e0619c34e4ef2db68610455444a456735f1f
Wataru Ono <wtr0414@gmail.com>
2021-10-19 23:56:54 +0900

    feat: 画面遷移するためのボタンを試しに追加
    Amplify の Preview 機能が適切に動作することを確認するため

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Button } from '@chakra-ui/react';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const TopPage = () => {
-  return <h1>TopPage</h1>;
+  const history = useHistory();
+
+  return (
+    <>
+      <h1>TopPage</h1>
+      <Button onClick={() => history.push('/login')}>ログイン画面</Button>
+      <Button onClick={() => history.push('/pull-requests/1')}>詳細画面</Button>
+      <Button onClick={() => history.push('/pull-requests/1/review')}>レビュー画面</Button>
+    </>
+  );
 };
 
 export default TopPage;
