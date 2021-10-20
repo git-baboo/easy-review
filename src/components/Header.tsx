5f662519887278050ac589db3119a5245f585d6a
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-20 16:00:30 +0900

    fix: レビューの部分の修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ import { BsFillCheckCircleFill } from 'react-icons/bs';
 
 const Header = () => {
   return (
-    <Flex alignItems="center" pl={16} height={36} bg="teal.500">
+    <Flex alignItems="center" pl={16} h={36} bg="teal.500">
       <HStack>
-        <Icon mb={6} as={BsFillCheckCircleFill} color="teal.600"></Icon>
+        <Icon mb={6} as={BsFillCheckCircleFill} color="teal.600" />
         <Box textColor="white">
           <Text>あなた宛にレビュー依頼が届いているよ👀</Text>
           <Text>レビューするプルリクエストを選んでみよう！</Text>
