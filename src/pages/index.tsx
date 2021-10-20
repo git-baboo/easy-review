e9ee365cb12fbadff7331259142d2e0798395f92
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-20 20:23:55 +0900

    feat: 動作確認を行うため適当な表示の記述

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { IconType } from 'react-icons';
+import { AiOutlineThunderbolt } from 'react-icons/ai';
+import { BiMessageAltDetail, BiAward } from 'react-icons/bi';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+
+// TODO: 後に削除  作業用にimportしているため
+import Layout from '@/components/Layout';
 
 const TopPage = () => {
-  return <h1>TopPage</h1>;
+  const pageNumber = Math.floor(Math.random() * 3);
+  console.log(pageNumber);
+  let text: string;
+  let icon: IconType;
+
+  if (pageNumber === 0) {
+    text = `あなた宛にレビュー依頼が届いているよ👀\nレビューするプルリクエストを選んでみよう！`;
+    icon = BsFillCheckCircleFill;
+  } else if (pageNumber === 1) {
+    text = `REVIEWボタンを押して\nさっそくレビューを開始しよう！`;
+    icon = AiOutlineThunderbolt;
+  } else if (pageNumber === 2) {
+    text = `テンプレートを使って\nレビューしてみよう！`;
+    icon = BiMessageAltDetail;
+  } else {
+    text = `極稀にみる確率だー！`;
+    icon = BiAward;
+  }
+
+  return (
+    <Layout text={text} icon={icon}>
+      <div>TopPage</div>
+    </Layout>
+  );
 };
 
 export default TopPage;
