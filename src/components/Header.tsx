4a0512bd3d62577110a1f484ed75970a991663da
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-20 14:08:22 +0900

    refactor: レイアウトの調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,18 @@
-import { Container, Text, Icon, HStack } from '@chakra-ui/react';
+import { Text, Icon, Flex, Box, HStack } from '@chakra-ui/react';
 import React from 'react';
 import { BiCheckCircle } from 'react-icons/bi';
 
-// type Props = () => {
-//   pageType: string;
-// };
-
 const Header = () => {
   return (
-    <HStack pl={16} height={36} bg="teal.500">
-      {/* 別途Containerを作ってflex-startをしないと、縦中央揃えがうまく作用しなかった*/}
-      <Container display="flex" alignItems="flex-start">
-        <Icon mt={1} as={BiCheckCircle}></Icon>
-        <Container display="flex" flexDirection="column" justifyContent="center" textColor="white">
+    <Flex alignItems="center" pl={16} height={36} bg="teal.500">
+      <HStack>
+        <Icon mb={6} as={BiCheckCircle}></Icon>
+        <Box textColor="white">
           <Text>あなた宛にレビュー依頼が届いているよ👀</Text>
           <Text>レビューするプルリクエストを選んでみよう！</Text>
-        </Container>
-      </Container>
-    </HStack>
+        </Box>
+      </HStack>
+    </Flex>
   );
 };
 
