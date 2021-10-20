16cb62c285e13eab193c078e26b6b3282cd45f89
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 07:49:49 +0900

    refactor: 作業用の部分の削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,11 @@
 import React from 'react';
-import { IconType } from 'react-icons';
-import { BiMessageAltDetail, BiAward } from 'react-icons/bi';
-import { BsFillCheckCircleFill, BsLightningCharge } from 'react-icons/bs';
+import { BiMessageAltDetail } from 'react-icons/bi';
 
-// 6行目の空いてる1行はeslint が自動生成して、消せなかった
 import Layout from '@/components/Layout';
 
 const TopPage = () => {
-  // TODO: 11行目～27行目は動作確認用で後に削除する
-  const pageNumber = Math.floor(Math.random() * 3);
-  let text: string;
-  let icon: IconType;
-
-  if (pageNumber === 0) {
-    text = `あなた宛にレビュー依頼が届いているよ👀\nレビューするプルリクエストを選んでみよう！`;
-    icon = BsFillCheckCircleFill;
-  } else if (pageNumber === 1) {
-    text = `REVIEWボタンを押して\nさっそくレビューを開始しよう！`;
-    icon = BsLightningCharge;
-  } else if (pageNumber === 2) {
-    text = `テンプレートを使ってレビューしてみよう！`;
-    icon = BiMessageAltDetail;
-  } else {
-    text = `極稀にみる確率だー！`;
-    icon = BiAward;
-  }
-
   return (
-    <Layout text={text} icon={icon}>
+    <Layout text={`テンプレートを使ってレビューしてみよう！`} icon={BiMessageAltDetail}>
       <h1>TopPage</h1>
     </Layout>
   );
