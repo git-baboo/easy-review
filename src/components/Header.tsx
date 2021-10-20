2d3a2dc64bc142799693164853ca93db34ccebc6
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-20 14:13:13 +0900

    fix: figmaに合うようにアイコンの修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { Text, Icon, Flex, Box, HStack } from '@chakra-ui/react';
 import React from 'react';
-import { BiCheckCircle } from 'react-icons/bi';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
 
 const Header = () => {
   return (
     <Flex alignItems="center" pl={16} height={36} bg="teal.500">
       <HStack>
-        <Icon mb={6} as={BiCheckCircle}></Icon>
+        <Icon mb={6} as={BsFillCheckCircleFill} color="gray.400"></Icon>
         <Box textColor="white">
           <Text>あなた宛にレビュー依頼が届いているよ👀</Text>
           <Text>レビューするプルリクエストを選んでみよう！</Text>
