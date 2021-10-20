cac0c11d84482cb4d5e1326e835039e3cc0fbdc0
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-20 19:32:56 +0900

    refactor: 作業用に作っていた部分の削除

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Header from '@/components/Header';
 import TopPage from '@/pages';
 import DetailPage from '@/pages/detail';
 import LoginPage from '@/pages/login';
@@ -12,8 +11,6 @@ const Router = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={TopPage} />
-        {/* TODO: 動作確認用に追加しているため、確認後は要削除*/}
-        <Route exact path="/header" component={Header} />
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/pull-requests/:id" component={DetailPage} />
         <Route exact path="/pull-requests/:id/review" component={ReviewPage} />
