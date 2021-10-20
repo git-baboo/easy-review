9b4f9c720ad3cce80f61c0b7bf7e268071dc9195
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-20 12:52:00 +0900

    feat: ログインボタンの作成
    ログインできるボタンが必要なため

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,16 @@
+import { Button } from '@chakra-ui/react';
 import React from 'react';
+import { DiGithubBadge } from 'react-icons/di';
 
 const LoginPage = () => {
-  return <h1>LoginPage</h1>;
+  return (
+    <>
+      <h1>LoginPage</h1>
+      <Button leftIcon={<DiGithubBadge color="black" size="30" />} colorScheme="blue">
+        <a style={{ color: 'black' }}>ログイン</a>
+      </Button>
+    </>
+  );
 };
 
 export default LoginPage;
