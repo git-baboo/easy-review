d22af1cd49a0de3cefda8b085f8f65feefc1cdeb
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-20 13:47:00 +0000

    feat: 詳細ページに必要な情報を表示させる
    必要な情報(コンポーネント)を確認するため、レイアウトは無視し、とりあえず表示

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,15 +1,26 @@
+import { Image } from '@chakra-ui/image';
+import { Heading } from '@chakra-ui/layout';
+import { Text } from '@chakra-ui/react';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import TimelineItem from '@/components/detail/TimelineItem';
-import { dummyComment as comment } from '@/data/dummyComment';
+import { pullRequest } from '@/data/dummyPullRequest';
 
 const DetailPage = () => {
+  // TODO: eslint の例外設定を削除
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { id } = useParams<{ id: string }>();
+
   return (
     <>
-      <h1>{id}</h1>
-      <TimelineItem comment={comment} />
+      <Text>
+        {pullRequest.repoOwner}/{pullRequest.repoName}
+      </Text>
+      <Heading>{pullRequest.title}</Heading>
+      <Image src={pullRequest.avatarUrl} />
+      <Text>{pullRequest.userName}</Text>
+      <TimelineItem comment={pullRequest.comment} />
     </>
   );
 };
