51def84a41fab290264e3faeec9f79d3032ba0b1
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 07:53:47 +0900

    refactor: 例示の修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { BiMessageAltDetail } from 'react-icons/bi';
+// import { BiMessageAltDetail } from 'react-icons/bi';
+import { BsLightningCharge } from 'react-icons/bs';
 
+// 6行目の空いてる1行はeslint が自動生成して、消せなかった
 import Layout from '@/components/Layout';
 
 const TopPage = () => {
   return (
-    <Layout text={`テンプレートを使ってレビューしてみよう！`} icon={BiMessageAltDetail}>
+    <Layout text={`REVIEWボタンを押して\nさっそくレビューを開始しよう！`} icon={BsLightningCharge}>
       <h1>TopPage</h1>
     </Layout>
   );
