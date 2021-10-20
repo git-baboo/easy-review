6c9d004eb0a11217ead983289474465a0d9f5469
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-20 19:24:01 +0900

    feat: テキストとアイコンの変数化

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import { BsFillCheckCircleFill } from 'react-icons/bs';
 
 const Header = () => {
+  const text = 'あなた宛にレビュー依頼が届いているよ👀\nレビューするプルリクエストを選んでみよう！';
+  const icon = BsFillCheckCircleFill;
+
   return (
     <Flex alignItems="center" pl={16} h={36} bg="teal.500">
       <HStack>
-        <Icon mb={6} as={BsFillCheckCircleFill} color="teal.600" />
-        <Text
-          color="white"
-          whiteSpace="pre-line"
-        >{`あなた宛にレビュー依頼が届いているよ👀\nレビューするプルリクエストを選んでみよう！`}</Text>
+        <Icon mb={6} as={icon} color="teal.600" />
+        <Text color="white" whiteSpace="pre-line">
+          {text}
+        </Text>
       </HStack>
     </Flex>
   );
