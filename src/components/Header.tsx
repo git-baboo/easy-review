c9adff1b07b5ded5612cf8e2461ed9e2a15f455b
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-20 13:19:00 +0900

    feat: アイコンを含めたレイアウトの調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,20 @@
-import { Container, Text } from '@chakra-ui/react';
+import { Container, Text, Icon, HStack } from '@chakra-ui/react';
 import React from 'react';
+import { BiBadgeCheck } from 'react-icons/bi';
 
 const Header = () => {
   return (
-    <Container
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      pl={16}
-      height={36}
-      textColor="white"
-      bg="teal.500"
-    >
-      <Text>今日のご飯何にしよう</Text>
-      <Text>焼肉食べたいなー</Text>
-    </Container>
+    <HStack pl={16} height={36} bg="teal.500">
+      {/* 別途Containerを作ってflex-startをしないと、縦中央揃えがうまく作用しなかった*/}
+      <Container display="flex" alignItems="flex-start">
+        <Icon as={BiBadgeCheck}></Icon>
+        <Container display="flex" flexDirection="column" justifyContent="center" textColor="white">
+          <Text>今日のご飯何にしよう</Text>
+          <Text>焼肉食べたいなー</Text>
+          <Text>おすしもいいなー</Text>
+        </Container>
+      </Container>
+    </HStack>
   );
 };
 
