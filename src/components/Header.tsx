5a7e6705df45de3347cc3309689c97aaab42b760
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-20 13:30:41 +0900

    refactor: レイアウトの微調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
     <HStack pl={16} height={36} bg="teal.500">
       {/* 別途Containerを作ってflex-startをしないと、縦中央揃えがうまく作用しなかった*/}
       <Container display="flex" alignItems="flex-start">
-        <Icon as={BiCheckCircle}></Icon>
+        <Icon mt={1} as={BiCheckCircle}></Icon>
         <Container display="flex" flexDirection="column" justifyContent="center" textColor="white">
           <Text>あなた宛にレビュー依頼が届いているよ👀</Text>
           <Text>レビューするプルリクエストを選んでみよう！</Text>
