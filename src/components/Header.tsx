fbbb06808bb1ae57637271d23e416cc8505adb5d
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-20 13:29:11 +0900

    fix: アイコンの変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,20 @@
 import { Container, Text, Icon, HStack } from '@chakra-ui/react';
 import React from 'react';
-import { BiBadgeCheck } from 'react-icons/bi';
+import { BiCheckCircle } from 'react-icons/bi';
+
+// type Props = () => {
+//   pageType: string;
+// };
 
 const Header = () => {
   return (
     <HStack pl={16} height={36} bg="teal.500">
       {/* 別途Containerを作ってflex-startをしないと、縦中央揃えがうまく作用しなかった*/}
       <Container display="flex" alignItems="flex-start">
-        <Icon as={BiBadgeCheck}></Icon>
+        <Icon as={BiCheckCircle}></Icon>
         <Container display="flex" flexDirection="column" justifyContent="center" textColor="white">
-          <Text>今日のご飯何にしよう</Text>
-          <Text>焼肉食べたいなー</Text>
-          <Text>おすしもいいなー</Text>
+          <Text>あなた宛にレビュー依頼が届いているよ👀</Text>
+          <Text>レビューするプルリクエストを選んでみよう！</Text>
         </Container>
       </Container>
     </HStack>
