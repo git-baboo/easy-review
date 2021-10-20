5f8635c887cf2be2d74f000c1cfa778b183777d6
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-20 13:56:35 +0900

    style: 配色の変更
    背景色とボタンの色を変更するため

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,8 +5,13 @@ import { DiGithubBadge } from 'react-icons/di';
 const LoginPage = () => {
   return (
     <>
+      <style>{'body{background-color:teal}'}</style>
       <h1>LoginPage</h1>
-      <Button leftIcon={<DiGithubBadge color="black" size="30" />} colorScheme="blue">
+      <Button
+        leftIcon={<DiGithubBadge color="black" size="25" />}
+        color="white"
+        borderColor="white"
+      >
         <a style={{ color: 'black' }}>ログイン</a>
       </Button>
     </>
