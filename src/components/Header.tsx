5c7c2329ceddcd8d2e44911bbdeb2f9fab6799a2
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-20 16:14:57 +0900

    refactor: コードをより簡潔に変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Text, Icon, Flex, Box, HStack } from '@chakra-ui/react';
+import { Text, Icon, Flex, HStack } from '@chakra-ui/react';
 import React from 'react';
 import { BsFillCheckCircleFill } from 'react-icons/bs';
 
@@ -7,10 +7,11 @@ const Header = () => {
     <Flex alignItems="center" pl={16} h={36} bg="teal.500">
       <HStack>
         <Icon mb={6} as={BsFillCheckCircleFill} color="teal.600" />
-        <Box color="white">
-          <Text>あなた宛にレビュー依頼が届いているよ👀</Text>
-          <Text>レビューするプルリクエストを選んでみよう！</Text>
-        </Box>
+
+        <Text
+          color="white"
+          whiteSpace="pre-line"
+        >{`あなた宛にレビュー依頼が届いているよ👀\nレビューするプルリクエストを選んでみよう！`}</Text>
       </HStack>
     </Flex>
   );
