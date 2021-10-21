e7967452eb4e3046c6f2705d2fd51522bb80d90d
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 16:51:46 +0900

    refactor: コードをより簡潔に変更

--- src/components/toppage/PullRequestList.tsx
+++ src/components/toppage/PullRequestList.tsx
@@ -3,24 +3,22 @@ import React from 'react';
 
 import { dummyPullRequests } from '@/data/dummyPullRequests';
 
-const PullRequestList = () => {
-  return (
-    <>
+const PullRequestList = () => (
+  <>
+    <VStack align="stretch" mt={6} mb={6} space={6}>
       {/* TODO: API から取得したデータに置き換える */}
-      {dummyPullRequests.map((pullRequest) => {
-        return (
-          <VStack key={pullRequest.title} mt={6} mb={6} align="stretch">
-            <Box fontSize={20} fontWeight="bold">
-              {pullRequest.title}
-            </Box>
-            <Box fontSize="xs">
-              {pullRequest.ownerName}/{pullRequest.repoName}
-            </Box>
-          </VStack>
-        );
-      })}
-    </>
-  );
-};
+      {dummyPullRequests.map((pullRequest) => (
+        <Box key={pullRequest.title}>
+          <Box fontSize={20} fontWeight="bold">
+            {pullRequest.title}
+          </Box>
+          <Box fontSize="xs">
+            {pullRequest.ownerName}/{pullRequest.repoName}
+          </Box>
+        </Box>
+      ))}
+    </VStack>
+  </>
+);
 
 export default PullRequestList;
