2cb74f7ed6f7a01200f699e740cb38529e68fad6
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-21 17:09:33 +0900

    feat: Templateのコンポーネントを作成

--- src/pages/review.tsx
+++ src/pages/review.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Template from '@/components/Template';
+
 const ReviewPage = () => {
-  return <h1>ReviewPage</h1>;
+  return (
+    <>
+      <h1>ReviewPage</h1>
+      <Template />
+    </>
+  );
 };
 
 export default ReviewPage;
