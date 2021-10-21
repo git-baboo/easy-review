a44048d4eba57e63c84e59c76dea2e6fc9c19e86
Wataru Ono <wtr0414@gmail.com>
2021-10-21 21:14:01 +0900

    feat: 認証機能の実装
    認証済みユーザにサービスを提供するため

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -2,13 +2,18 @@ import { Button } from '@chakra-ui/react';
 import React from 'react';
 import { DiGithubBadge } from 'react-icons/di';
 
+import { useAuth } from '@/hooks/useAuth';
+
 const LoginButton = () => {
+  const { login } = useAuth();
+
   return (
     <Button
       leftIcon={<DiGithubBadge color="black" size="25" />}
       bgColor="white"
       color="black"
       mt={8}
+      onClick={login}
     >
       ログイン
     </Button>
