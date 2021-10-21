f96d94dd4355cd19e37d724227b988f9df51f3ca
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 19:39:50 +0900

    fix: 余白部分の酒精

--- src/components/toppage/PullRequestList.tsx
+++ src/components/toppage/PullRequestList.tsx
@@ -7,7 +7,7 @@ const PullRequestList = () => (
   <VStack align="stretch" my={6} spacing={6} divider={<StackDivider />}>
     {/* TODO: API から取得したデータに置き換える */}
     {dummyPullRequests.map((pullRequest, index) => (
-      <Box key={index}>
+      <Box key={index} mx={8}>
         <Heading size="md">{pullRequest.title}</Heading>
         <Text size="xs">
           {pullRequest.ownerName}/{pullRequest.repoName}
