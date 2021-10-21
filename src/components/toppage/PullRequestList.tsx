842ceffa9e90dc813e8881b46692f10995049b54
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 19:49:35 +0900

    feat: Boxが押されることを検知する機能の実装

--- src/components/toppage/PullRequestList.tsx
+++ src/components/toppage/PullRequestList.tsx
@@ -3,18 +3,30 @@ import React from 'react';
 
 import { dummyPullRequests } from '@/data/dummyPullRequests';
 
-const PullRequestList = () => (
-  <VStack align="stretch" my={6} spacing={6} divider={<StackDivider />}>
-    {/* TODO: API から取得したデータに置き換える */}
-    {dummyPullRequests.map((pullRequest, index) => (
-      <Box key={index} mx={8}>
-        <Heading size="md">{pullRequest.title}</Heading>
-        <Text size="xs">
-          {pullRequest.ownerName}/{pullRequest.repoName}
-        </Text>
-      </Box>
-    ))}
-  </VStack>
-);
+type PullRequest = {
+  ownerName: string;
+  repoName: string;
+  title: string;
+};
+
+const PullRequestList = () => {
+  const handleClick = (pullRequest: PullRequest) => {
+    console.log(pullRequest);
+  };
+
+  return (
+    <VStack align="stretch" my={6} spacing={6} divider={<StackDivider />}>
+      {/* TODO: API から取得したデータに置き換える */}
+      {dummyPullRequests.map((pullRequest, index) => (
+        <Box key={index} mx={8} onClick={() => handleClick(pullRequest)}>
+          <Heading size="md">{pullRequest.title}</Heading>
+          <Text size="xs" color="gray.600" S>
+            {pullRequest.ownerName}/{pullRequest.repoName}
+          </Text>
+        </Box>
+      ))}
+    </VStack>
+  );
+};
 
 export default PullRequestList;
