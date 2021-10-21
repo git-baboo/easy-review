1a16947afce02ac627a249f88f980b86ab56bb61
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-21 07:48:44 +0000

    feat: review ボタンにレビュー画面への遷移を実装

--- src/components/detail/ReviewButton.tsx
+++ src/components/detail/ReviewButton.tsx
@@ -1,10 +1,21 @@
 import { Button } from '@chakra-ui/button';
+import { useHistory } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 const ReviewButton = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    const currentPath = history.location.pathname;
+    history.push(currentPath + '/review');
+  };
+
   return (
-    <Button size="lg" w="full" bgColor="teal.500" color="white">
-      REVIEW
-    </Button>
+    <BrowserRouter>
+      <Button size="lg" w="full" bgColor="teal.500" color="white" onClick={handleClick}>
+        REVIEW
+      </Button>
+    </BrowserRouter>
   );
 };
 
