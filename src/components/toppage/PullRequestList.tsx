c82857fdcbd8132197bebef5202e263d3586c796
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 16:57:52 +0900

    refactor: フラグメントの削除

--- src/components/toppage/PullRequestList.tsx
+++ src/components/toppage/PullRequestList.tsx
@@ -4,21 +4,19 @@ import React from 'react';
 import { dummyPullRequests } from '@/data/dummyPullRequests';
 
 const PullRequestList = () => (
-  <>
-    <VStack align="stretch" mt={6} mb={6} space={6}>
-      {/* TODO: API から取得したデータに置き換える */}
-      {dummyPullRequests.map((pullRequest) => (
-        <Box key={pullRequest.title}>
-          <Box fontSize={20} fontWeight="bold">
-            {pullRequest.title}
-          </Box>
-          <Box fontSize="xs">
-            {pullRequest.ownerName}/{pullRequest.repoName}
-          </Box>
+  <VStack align="stretch" mt={6} mb={6} space={6}>
+    {/* TODO: API から取得したデータに置き換える */}
+    {dummyPullRequests.map((pullRequest) => (
+      <Box key={pullRequest.title}>
+        <Box fontSize={20} fontWeight="bold">
+          {pullRequest.title}
         </Box>
-      ))}
-    </VStack>
-  </>
+        <Box fontSize="xs">
+          {pullRequest.ownerName}/{pullRequest.repoName}
+        </Box>
+      </Box>
+    ))}
+  </VStack>
 );
 
 export default PullRequestList;
