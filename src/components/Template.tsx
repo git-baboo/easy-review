1d24534366a4c5ada4d91a9c3b6eb2f441f05a1c
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-21 18:20:52 +0900

    style: テンプレート間の間隔を変更

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,30 +1,36 @@
-import { Text, Container } from '@chakra-ui/react';
+import { Text, Stack, Box } from '@chakra-ui/react';
 import React from 'react';
 
 const Template = () => {
   return (
     <>
-      <Container>
-        <Text color="black" size="semibold">
-          ❓質問
-        </Text>
-        <Text color="black">
-          わからないコードや疑問に思った部分があれば積極的に質問してみましょう！
-        </Text>
-        <Text color="black" size="semibold">
-          ✨素敵
-        </Text>
-        <Text color="black">
-          命名や設計がキレイなどの良いコードを見つけたときはポジティブな意見をバシバシ送っちゃいましょう！
-          プルリクエストの作成者は嬉しいこと間違いなしです
-        </Text>
-        <Text color="black" size="semibold">
-          🤔指摘
-        </Text>
-        <Text color="black">
-          タイポや可読性の低いコードを見つけた場合は早急に報告しましょう！あなたの報告がプロダクトを改善します！
-        </Text>
-      </Container>
+      <Stack spacing={5}>
+        <Box>
+          <Text color="black" size="semibold">
+            ❓質問
+          </Text>
+          <Text color="black">
+            わからないコードや疑問に思った部分があれば積極的に質問してみましょう！
+          </Text>
+        </Box>
+        <Box>
+          <Text color="black" size="semibold">
+            ✨素敵
+          </Text>
+          <Text color="black">
+            命名や設計がキレイなどの良いコードを見つけたときはポジティブな意見をバシバシ送っちゃいましょう！
+            プルリクエストの作成者は嬉しいこと間違いなしです
+          </Text>
+        </Box>
+        <Box>
+          <Text color="black" size="semibold">
+            🤔指摘
+          </Text>
+          <Text color="black">
+            タイポや可読性の低いコードを見つけた場合は早急に報告しましょう！あなたの報告がプロダクトを改善します！
+          </Text>
+        </Box>
+      </Stack>
     </>
   );
 };
