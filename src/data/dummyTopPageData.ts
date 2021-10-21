8c479a5e2f8824843505652e81f1cbc8329c4825
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 14:35:03 +0900

    feat: ダミーデータの作成

--- src/data/dummyTopPageData.ts
+++ src/data/dummyTopPageData.ts
@@ -9,7 +9,23 @@ const DummyTopPageData = () => {
     directory: 'git-baboo/minihackathon-a',
   };
 
-  return data1;
+  const data2: DummyDataTmp = {
+    title: 'Feature/7 change todo state',
+    directory: 'git-baboo/react-study-sugimizu',
+  };
+
+  const data3: DummyDataTmp = {
+    title: '認証機能の実装',
+    directory: 'zemi-team2/sake',
+  };
+
+  const dataset = [];
+
+  dataset.push(data1);
+  dataset.push(data2);
+  dataset.push(data3);
+
+  return dataset;
 };
 
 export default DummyTopPageData;
