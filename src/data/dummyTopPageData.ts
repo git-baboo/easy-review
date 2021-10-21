69b45cba2d0f57bc6bbed765c37fbcd6b9188b14
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 13:15:52 +0900

    feat: ダミーデータの作成

---
+++ src/data/dummyTopPageData.ts
@@ -0,0 +1,15 @@
+const DummyTopPageData = () => {
+  type DummyDataTmp = {
+    title: string;
+    directory: string;
+  };
+
+  const data1: DummyDataTmp = {
+    title: 'ToDoのstate管理方法の変更',
+    directory: 'git-baboo/minihackathon-a',
+  };
+
+  return data1;
+};
+
+export default DummyTopPageData;
