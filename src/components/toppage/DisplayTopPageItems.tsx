cf34ca9d48ed4434974bdb21c57cb8220cb5b7b9
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 14:36:09 +0900

    feat: データを変数として表示できるように変更

--- src/components/toppage/DisplayTopPageItems.tsx
+++ src/components/toppage/DisplayTopPageItems.tsx
@@ -5,34 +5,20 @@ import DummyTopPageData from '@/data/dummyTopPageData';
 
 const DisplayTopPageItems = () => {
   const dataset = DummyTopPageData();
+
   return (
     <>
-      <Box mt={6} mb={6} bg="green.100">
-        <Text fontSize="md" fontWeight="bold">
-          {dataset.title}
-        </Text>
-        <Text fontSize="xs">{dataset.directory}</Text>
-      </Box>
-      {/* <Box mt={6} mb={6} bg="green.100">
-        <Text fontSize="md" fontWeight="bold">
-          ToDoのstate管理方法の変更
-        </Text>
-        <Text fontSize="xs">git-baboo/minihackathon-a</Text>
-      </Box>
-      <Box mt={6} mb={6} bg="green.100">
-        <Text fontSize="md" fontWeight="bold">
-          ToDoのstate管理方法の変更
-        </Text>
-        <Text fontSize="xs">git-baboo/minihackathon-a</Text>
-      </Box>
-      <Box mt={6} mb={6} bg="green.100">
-        <Text fontSize="md" fontWeight="bold">
-          ToDoのstate管理方法の変更
-        </Text>
-        <Text fontSize="xs">git-baboo/minihackathon-a</Text>
-      </Box> */}
+      {dataset.map((data) => {
+        return (
+          <Box key={data.title} mt={6} mb={6}>
+            <Text fontSize="md" fontWeight="bold">
+              {data.title}
+            </Text>
+            <Text fontSize="xs">{data.directory}</Text>
+          </Box>
+        );
+      })}
     </>
   );
 };
-
 export default DisplayTopPageItems;
