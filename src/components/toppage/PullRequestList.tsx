fd5d6495e5b7953a930a29c89016e01ca71fdcca
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 17:32:15 +0900

    refactor: フォント等の修正

--- src/components/toppage/PullRequestList.tsx
+++ src/components/toppage/PullRequestList.tsx
@@ -1,22 +1,26 @@
-import { Box, VStack } from '@chakra-ui/react';
+import { Box, VStack, Heading, Text } from '@chakra-ui/react';
 import React from 'react';
 
 import { dummyPullRequests } from '@/data/dummyPullRequests';
 
-const PullRequestList = () => (
-  <VStack align="stretch" mt={6} mb={6} space={6}>
-    {/* TODO: API から取得したデータに置き換える */}
-    {dummyPullRequests.map((pullRequest) => (
-      <Box key={pullRequest.title}>
-        <Box fontSize={20} fontWeight="bold">
-          {pullRequest.title}
-        </Box>
-        <Box fontSize="xs">
-          {pullRequest.ownerName}/{pullRequest.repoName}
-        </Box>
-      </Box>
-    ))}
-  </VStack>
-);
+const PullRequestList = () => {
+  return (
+    <>
+      <VStack align="stretch" mt={6} mb={6} spacing={6}>
+        {/* TODO: API から取得したデータに置き換える */}
+        {dummyPullRequests.map((pullRequest) => {
+          return (
+            <Box key={pullRequest.title}>
+              <Heading size="md">{pullRequest.title}</Heading>
+              <Text size="xs">
+                {pullRequest.ownerName}/{pullRequest.repoName}
+              </Text>
+            </Box>
+          );
+        })}
+      </VStack>
+    </>
+  );
+};
 
 export default PullRequestList;
