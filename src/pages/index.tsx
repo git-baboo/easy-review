fe816aaaaf82d9cf97476f46fad3ab7a8a0608d8
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 10:10:56 +0900

    fix: 不要なimport の削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-// import { BiMessageAltDetail } from 'react-icons/bi';
 import { BsLightningCharge } from 'react-icons/bs';
 
-// 6行目の空いてる1行はeslint が自動生成して、消せなかった
 import Layout from '@/components/Layout';
 
 const TopPage = () => {
