bbfc0ec05d667ae244b5024f1f701fe745f8bd42
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-21 20:07:37 +0900

    style: Stackの位置を指定している箇所を削除

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -4,29 +4,29 @@ import React from 'react';
 const Template = () => {
   return (
     <>
-      <VStack spacing={5} ml={853} mt={264}>
+      <VStack spacing={5}>
         <Box width={235}>
-          <Text color="black" fontSize="lg" fontStyle="semibold">
+          <Text color="black" fontSize="lg" fontWeight="semibold">
             ❓質問
           </Text>
-          <Text color="black" fontSize="xs" fontStyle="medium">
+          <Text color="black" fontSize="xs" fontWeight="medium">
             わからないコードや疑問に思った部分があれば積極的に質問してみましょう！
           </Text>
         </Box>
         <Box width={235}>
-          <Text color="black" fontSize="lg" fontStyle="semibold">
+          <Text color="black" fontSize="lg" fontWeight="semibold">
             ✨素敵
           </Text>
-          <Text color="black" fontSize="xs" fontStyle="medium">
+          <Text color="black" fontSize="xs" fontWeight="medium">
             命名や設計がキレイなどの良いコードを見つけたときはポジティブな意見をバシバシ送っちゃいましょう！
             プルリクエストの作成者は嬉しいこと間違いなしです
           </Text>
         </Box>
         <Box width={235}>
-          <Text color="black" fontSize="lg" fontStyle="semibold">
+          <Text color="black" fontSize="lg" fontWeight="semibold">
             🤔改善
           </Text>
-          <Text color="black" fontSize="xs" fontStyle="medium">
+          <Text color="black" fontSize="xs" fontWeight="medium">
             タイポや可読性の低いコードを見つけた場合は早急に報告しましょう！あなたの報告がプロダクトを改善します！
           </Text>
         </Box>
