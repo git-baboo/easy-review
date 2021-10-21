a5bc025e54d41aa8a822ad4a8db822c26888c431
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-21 17:39:39 +0900

    style: フォントの変更

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,28 +1,30 @@
-import { Heading, Text } from '@chakra-ui/react';
+import { Text, Container } from '@chakra-ui/react';
 import React from 'react';
 
 const Template = () => {
   return (
     <>
-      <Heading color="black" size="sm">
-        ❓質問
-      </Heading>
-      <Text color="black">
-        わからないコードや疑問に思った部分があれば積極的に質問してみましょう！
-      </Text>
-      <Heading color="black" size="sm">
-        ✨素敵
-      </Heading>
-      <Text color="black">
-        命名や設計がキレイなどの良いコードを見つけたときはポジティブな意見をバシバシ送っちゃいましょう！
-        プルリクエストの作成者は嬉しいこと間違いなしです
-      </Text>
-      <Heading color="black" size="sm">
-        🤔指摘
-      </Heading>
-      <Text color="black">
-        タイポや可読性の低いコードを見つけた場合は早急に報告しましょう！あなたの報告がプロダクトを改善します！
-      </Text>
+      <Container>
+        <Text color="black" size="semibold">
+          ❓質問
+        </Text>
+        <Text color="black">
+          わからないコードや疑問に思った部分があれば積極的に質問してみましょう！
+        </Text>
+        <Text color="black" size="semibold">
+          ✨素敵
+        </Text>
+        <Text color="black">
+          命名や設計がキレイなどの良いコードを見つけたときはポジティブな意見をバシバシ送っちゃいましょう！
+          プルリクエストの作成者は嬉しいこと間違いなしです
+        </Text>
+        <Text color="black" size="semibold">
+          🤔指摘
+        </Text>
+        <Text color="black">
+          タイポや可読性の低いコードを見つけた場合は早急に報告しましょう！あなたの報告がプロダクトを改善します！
+        </Text>
+      </Container>
     </>
   );
 };
