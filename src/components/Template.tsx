17ff376287c12b4f57f752cd146e964f28a10aab
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-21 19:40:17 +0900

    style: Stackの位置を変更

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,36 +1,36 @@
-import { Text, Stack, Box } from '@chakra-ui/react';
+import { Text, VStack, Box } from '@chakra-ui/react';
 import React from 'react';
 
 const Template = () => {
   return (
     <>
-      <Stack spacing={5}>
+      <VStack spacing={5} ml={853} mt={264}>
         <Box width={235}>
-          <Text color="black" size="semibold">
+          <Text color="black" size="lg" font="semibold">
             ❓質問
           </Text>
-          <Text color="black">
+          <Text color="black" size="xs" font="medium">
             わからないコードや疑問に思った部分があれば積極的に質問してみましょう！
           </Text>
         </Box>
         <Box width={235}>
-          <Text color="black" size="semibold">
+          <Text color="black" size="lg" font="semibold">
             ✨素敵
           </Text>
-          <Text color="black">
+          <Text color="black" size="xs" font="medium">
             命名や設計がキレイなどの良いコードを見つけたときはポジティブな意見をバシバシ送っちゃいましょう！
             プルリクエストの作成者は嬉しいこと間違いなしです
           </Text>
         </Box>
         <Box width={235}>
-          <Text color="black" size="semibold">
+          <Text color="black" size="lg" font="semibold">
             🤔指摘
           </Text>
-          <Text color="black">
+          <Text color="black" size="xs" font="medium">
             タイポや可読性の低いコードを見つけた場合は早急に報告しましょう！あなたの報告がプロダクトを改善します！
           </Text>
         </Box>
-      </Stack>
+      </VStack>
     </>
   );
 };
