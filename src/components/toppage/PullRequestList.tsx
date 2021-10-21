141529454abf5e36200affda56981b362a8b3fab
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 20:31:41 +0900

    fix: クリック認識の領域を変更

--- src/components/toppage/PullRequestList.tsx
+++ src/components/toppage/PullRequestList.tsx
@@ -16,11 +16,13 @@ const PullRequestList = () => {
     <VStack align="stretch" my={6} spacing={6} divider={<StackDivider />}>
       {/* TODO: API から取得したデータに置き換える */}
       {dummyPullRequests.map((pullRequest, index) => (
-        <Box key={index} mx={8} onClick={() => handleClick(pullRequest)}>
-          <Heading size="md">{pullRequest.title}</Heading>
-          <Text fontSize="xs" color="gray.600" S>
-            {pullRequest.ownerName}/{pullRequest.repoName}
-          </Text>
+        <Box key={index} mx={8}>
+          <Box display="inline-block" onClick={() => handleClick(pullRequest)}>
+            <Heading size="md">{pullRequest.title}</Heading>
+            <Text fontSize="xs" color="gray.600">
+              {pullRequest.ownerName}/{pullRequest.repoName}
+            </Text>
+          </Box>
         </Box>
       ))}
     </VStack>
