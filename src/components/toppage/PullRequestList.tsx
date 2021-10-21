fbfc284b0f55da0e7317b3b0cafae3577177a245
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 19:05:12 +0900

    fix: レビューを受けての変更

--- src/components/toppage/PullRequestList.tsx
+++ src/components/toppage/PullRequestList.tsx
@@ -1,13 +1,13 @@
-import { Box, VStack, Heading, Text } from '@chakra-ui/react';
+import { Box, VStack, Heading, Text, StackDivider } from '@chakra-ui/react';
 import React from 'react';
 
 import { dummyPullRequests } from '@/data/dummyPullRequests';
 
 const PullRequestList = () => (
-  <VStack align="stretch" mt={6} mb={6} spacing={6}>
+  <VStack align="stretch" my={6} spacing={6} divider={<StackDivider />}>
     {/* TODO: API から取得したデータに置き換える */}
-    {dummyPullRequests.map((pullRequest) => (
-      <Box key={pullRequest.title}>
+    {dummyPullRequests.map((pullRequest, index) => (
+      <Box key={index}>
         <Heading size="md">{pullRequest.title}</Heading>
         <Text size="xs">
           {pullRequest.ownerName}/{pullRequest.repoName}
