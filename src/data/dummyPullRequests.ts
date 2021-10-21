1fab3ecb14279ee3e5ed0a90398618b69a0ea527
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-21 20:10:32 +0900

    refactor: 型定義を外でも使えるように変更

--- src/data/dummyPullRequests.ts
+++ src/data/dummyPullRequests.ts
@@ -1,7 +1,8 @@
-type PullRequest = {
+export type PullRequest = {
   ownerName: string;
   repoName: string;
   title: string;
+  pullNumber: number;
 };
 
 export const dummyPullRequests: PullRequest[] = [
@@ -9,15 +10,18 @@ export const dummyPullRequests: PullRequest[] = [
     ownerName: 'git-baboo',
     repoName: 'minihackathon-a',
     title: 'ToDoのstate管理方法の変更',
+    pullNumber: 10,
   },
   {
     ownerName: 'git-baboo',
     repoName: 'react-study-sugimizu',
     title: 'Feature/7 change todo state',
+    pullNumber: 5,
   },
   {
     ownerName: 'zemi-team2',
     repoName: 'sake',
     title: '認証機能の実装',
+    pullNumber: 100,
   },
 ];
