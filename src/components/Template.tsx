0ff62d5ad77943bb0e4e6546596e57f9e2b5af3a
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-21 19:58:04 +0900

    style: fontサイズの変更

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -6,27 +6,27 @@ const Template = () => {
     <>
       <VStack spacing={5} ml={853} mt={264}>
         <Box width={235}>
-          <Text color="black" size="lg" font="semibold">
+          <Text color="black" fontSize="lg" fontStyle="semibold">
             ❓質問
           </Text>
-          <Text color="black" size="xs" font="medium">
+          <Text color="black" fontSize="xs" fontStyle="medium">
             わからないコードや疑問に思った部分があれば積極的に質問してみましょう！
           </Text>
         </Box>
         <Box width={235}>
-          <Text color="black" size="lg" font="semibold">
+          <Text color="black" fontSize="lg" fontStyle="semibold">
             ✨素敵
           </Text>
-          <Text color="black" size="xs" font="medium">
+          <Text color="black" fontSize="xs" fontStyle="medium">
             命名や設計がキレイなどの良いコードを見つけたときはポジティブな意見をバシバシ送っちゃいましょう！
             プルリクエストの作成者は嬉しいこと間違いなしです
           </Text>
         </Box>
         <Box width={235}>
-          <Text color="black" size="lg" font="semibold">
-            🤔指摘
+          <Text color="black" fontSize="lg" fontStyle="semibold">
+            🤔改善
           </Text>
-          <Text color="black" size="xs" font="medium">
+          <Text color="black" fontSize="xs" fontStyle="medium">
             タイポや可読性の低いコードを見つけた場合は早急に報告しましょう！あなたの報告がプロダクトを改善します！
           </Text>
         </Box>
