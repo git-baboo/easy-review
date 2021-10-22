7de6fb83426d1f7b6a65abda3e2099d770e56c8b
Wataru Ono <wtr0414@gmail.com>
2021-10-22 12:05:34 +0900

    feat: プルリクエストのデータを GitHub API から取得したのもに差し替え
    動的にデータを描画するため

--- src/components/toppage/PullRequestList.tsx
+++ src/components/toppage/PullRequestList.tsx
@@ -1,34 +1,82 @@
-import { Box, VStack, Heading, Text, StackDivider } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Heading, StackDivider, Text, VStack } from '@chakra-ui/react';
+import { Octokit } from '@octokit/rest';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 
-// TODO: ダミーデータのとこで作成した型を再利用しているため、後に変更が必要
-import { dummyPullRequests, PullRequest } from '@/data/dummyPullRequests';
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_TOKEN,
+});
+
+type Pull = {
+  pullNumber: number;
+  ownerName: string;
+  repoName: string;
+  title: string;
+};
 
 const PullRequestList = () => {
+  const [pulls, setPulls] = useState<Pull[]>([]);
   const history = useHistory();
-  const handleClick = (pullRequest: PullRequest) => {
+
+  useEffect(() => {
+    let isMounted = true;
+    octokit
+      .request('GET /search/issues', {
+        // TODO: ユーザ名をログイン中のユーザのものに変更する
+        q: 'is:pr+user-review-requested:watagit+state:open',
+      })
+      .then((response) => {
+        const items = response.data.items;
+        items.map((item) => {
+          const pullRequest: Pull = {
+            pullNumber: 0,
+            ownerName: '',
+            repoName: '',
+            title: '',
+          };
+          pullRequest.pullNumber = item.number;
+          console.log(item.number);
+          pullRequest.title = item.title;
+          octokit.request(`GET ${item.repository_url}`).then((response) => {
+            console.log(response.data.organization.login);
+            pullRequest.ownerName = response.data.organization.login;
+            pullRequest.repoName = response.data.name;
+            if (isMounted) {
+              setPulls([...pulls, pullRequest]);
+            }
+          });
+        });
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  const handleClick = (pullRequest: Pull) => {
     history.push(`/${pullRequest.ownerName}/${pullRequest.repoName}/${pullRequest.pullNumber}`);
   };
 
   return (
-    <VStack align="stretch" my={6} spacing={6} divider={<StackDivider />}>
-      {/* TODO: API から取得したデータに置き換える */}
-      {dummyPullRequests.map((pullRequest, index) => (
-        <Box key={index} mx={8}>
-          <Box
-            display="inline-block"
-            _hover={{ cursor: 'pointer' }}
-            onClick={() => handleClick(pullRequest)}
-          >
-            <Heading size="md">{pullRequest.title}</Heading>
-            <Text fontSize="xs" color="gray.600">
-              {pullRequest.ownerName}/{pullRequest.repoName}
-            </Text>
+    <>
+      <VStack align="stretch" my={6} spacing={6} divider={<StackDivider />}>
+        {/* TODO: API から取得したデータに置き換える */}
+        {pulls.map((pullRequest, index) => (
+          <Box key={index} mx={8}>
+            <Box
+              display="inline-block"
+              _hover={{ cursor: 'pointer' }}
+              onClick={() => handleClick(pullRequest)}
+            >
+              <Heading size="md">{pullRequest.title}</Heading>
+              <Text fontSize="xs" color="gray.600">
+                {pullRequest.ownerName}/{pullRequest.repoName}
+              </Text>
+            </Box>
           </Box>
-        </Box>
-      ))}
-    </VStack>
+        ))}
+      </VStack>
+    </>
   );
 };
 
