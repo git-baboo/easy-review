de4fe715ac35ca2197cab7becb786f86ac063c85
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-22 02:49:20 +0000

    chore: merge 忘れ

--- src/data/dummyPullRequest.ts
+++ src/data/dummyPullRequest.ts
@@ -6,145 +6,145 @@ export const pullRequest: PullRequest = {
   userName: 'kacha-122',
   avatarUrl: 'https://avatars.githubusercontent.com/u/68210096?v=4',
   comment: `
-    # 見出し1
-    ## 見出し2
-    ### 見出し3
-    #### 見出し4
-    ##### 見出し5
-    ###### 見出し6
+  # 見出し1
+  ## 見出し2
+  ### 見出し3
+  #### 見出し4
+  ##### 見出し5
+  ###### 見出し6
 
-      ---
-      ## スタイル付きテキスト
+  ---
+  ## スタイル付きテキスト
 
-      **太字**
+  **太字**
 
-      *斜体*
+  *斜体*
 
-      ~~打ち消し線~~
+  ~~打ち消し線~~
 
-      **強調**
+  **強調**
 
-      ***強調 + 斜体***
+  ***強調 + 斜体***
 
-      ---
-      ## テキストの引用
+  ---
+  ## テキストの引用
 
-      > 引用
-      >> 多重引用
+  > 引用
+  >> 多重引用
 
-      ---
-      ## コード
+  ---
+  ## コード
 
-      ### インライン
+  ### インライン
 
-      これは \`インラインコード\` です。
+  これは \`インラインコード\` です。
 
-      ### コードブロック
+  ### コードブロック
 
-      \`\`\`
-      git status
-      git add
-      git commit
-      \`\`\`
+  \`\`\`
+  git status
+  git add
+  git commit
+  \`\`\`
 
-      ### 言語識別子付き
+  ### 言語識別子付き
 
-      \`\`\`ts
-      ReactDOM.render(
-        <h1>Hello, world!</h1>,
-        document.getElementById('root')
-      );
-      \`\`\`
+  \`\`\`ts
+  ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
+  \`\`\`
 
-      ### Diff
+  ### Diff
 
-      \`\`\`diff
-      - old
-      + new
-      \`\`\`
+  \`\`\`diff
+  - old
+  + new
+  \`\`\`
 
 
-      ---
-      ## リスト
+  ---
+  ## リスト
 
-      - リスト1
-        - リスト1_1
-            - リスト1_1_1
-            - リスト1_1_2
-        - リスト1_2
-      - リスト2
-      - リスト3
+  - リスト1
+    - リスト1_1
+        - リスト1_1_1
+        - リスト1_1_2
+    - リスト1_2
+  - リスト2
+  - リスト3
 
-      1. 番号付きリスト1
-        1. 番号付きリスト1-1
-        1. 番号付きリスト1-2
-      1. 番号付きリスト2
-      1. 番号付きリスト3
+  1. 番号付きリスト1
+    1. 番号付きリスト1-1
+    1. 番号付きリスト1-2
+  1. 番号付きリスト2
+  1. 番号付きリスト3
 
-      ---
-      ## リンク
+  ---
+  ## リンク
 
-      [GitHub](https://github.com/)
+  [GitHub](https://github.com/)
 
-      ### リンク文字列 直書き
+  ### リンク文字列 直書き
 
-      https://github.com
+  https://github.com
 
-      ### 相対リンク
+  ### 相対リンク
 
-      [このプロジェクトへのコントリビューションガイドライン](docs/CONTRIBUTING.md)
+  [このプロジェクトへのコントリビューションガイドライン](docs/CONTRIBUTING.md)
 
-      ### コミットハッシュ
+  ### コミットハッシュ
 
-      ea18ba7a93bf3c75b554e5e73bf84621cd283956
+  ea18ba7a93bf3c75b554e5e73bf84621cd283956
 
-      ---
-      ## 折りたたみ
+  ---
+  ## 折りたたみ
 
-      <details>
-      <summary>Summary</summary>
-      details
-      </details>
+  <details>
+  <summary>Summary</summary>
+  details
+  </details>
 
-      ---
-      ## 表
+  ---
+  ## 表
 
-      | Left-aligned | Center-aligned | Right-aligned |
-      | :---         |     :---:      |          ---: |
-      | git status   | git status     | git status    |
-      | git diff     | git diff       | git diff      |
+  | Left-aligned | Center-aligned | Right-aligned |
+  | :---         |     :---:      |          ---: |
+  | git status   | git status     | git status    |
+  | git diff     | git diff       | git diff      |
 
-      ---
-      ## 画像
+  ---
+  ## 画像
 
-      ![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
+  ![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
 
-      ---
-      ## チェックボックス
+  ---
+  ## チェックボックス
 
-      - [ ] Todo
-      - [x] Done
+  - [ ] Todo
+  - [x] Done
 
-      ---
-      ## 人や Team のメンション
+  ---
+  ## 人や Team のメンション
 
-      @github/support これらのアップデートについてどう思いますか？
+  @github/support これらのアップデートについてどう思いますか？
 
-      ---
-      ## Issue, PullRequest の参照
+  ---
+  ## Issue, PullRequest の参照
 
-      #1
+  #1
 
-      ---
-      ## 脚注
+  ---
+  ## 脚注
 
-      Here is a simple footnote[^1].
+  Here is a simple footnote[^1].
 
-      [^1]: My reference.
+  [^1]: My reference.
 
-      ---
-      ## コメントアウト
+  ---
+  ## コメントアウト
 
-      <!-- コメントアウト -->
-    `,
+  <!-- コメントアウト -->
+  `,
 };
