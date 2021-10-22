f7b007dfaa403c9421a79a5f5ee4415bee3d35ae
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-22 12:53:48 +0900

    feat: 大まかな概観の作成

--- src/pages/review.tsx
+++ src/pages/review.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/layout';
+import { Box, Text, Container, HStack, Divider, Center } from '@chakra-ui/layout';
 import React from 'react';
 import { BsFillChatDotsFill } from 'react-icons/bs';
 
@@ -9,10 +9,29 @@ const ReviewPage = () => {
   return (
     <>
       <Layout text={'テンプレートを使ってレビューをしてみよう！'} icon={BsFillChatDotsFill}>
-        <Box />
+        <Container maxW="container.lg" color="white">
+          <HStack spacing={20} mt={9} align="top">
+            <Box width={700}>
+              <Text color="gray" fontSize="xs">
+                git-baboo/minihackathon-a
+              </Text>
+              <Text color="black" fontSize="md">
+                ToDoのstate管理方法の変更
+              </Text>
+              <Text color="black" fontSize="xs">
+                kacha-122
+              </Text>
+              <Text color="black" fontSize="xs">
+                以下コード
+              </Text>
+            </Box>
+            <Center height="lg">
+              <Divider color="black" orientation="vertical" />
+            </Center>
+            <TemplateList />
+          </HStack>
+        </Container>
       </Layout>
-
-      <TemplateList />
     </>
   );
 };
