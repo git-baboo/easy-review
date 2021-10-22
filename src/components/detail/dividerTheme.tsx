0212ac72a8f8200c5c8bbbb89c0e468496de36dc
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-22 09:40:31 +0000

    style: 区切り線のマージンを修正

--- src/components/detail/dividerTheme.tsx
+++ src/components/detail/dividerTheme.tsx
@@ -1,7 +1,7 @@
 import { Divider } from '@chakra-ui/layout';
 
 const dividerTheme = () => {
-  return <Divider borderColor="gray.300" borderWidth={2} />;
+  return <Divider borderColor="gray.300" borderWidth={2} my={4} />;
 };
 
 export default dividerTheme;
