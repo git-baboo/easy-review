f933ece43d231769c524d449a78dbadf60df8ce5
Wataru Ono <wtr0414@gmail.com>
2021-10-22 17:36:14 +0900

    feat: API から取得したデータに置き換え
    動的にデータを描画するため

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,5 +1,6 @@
 import { Container, Heading, Text } from '@chakra-ui/layout';
-import React from 'react';
+import { Octokit } from '@octokit/rest';
+import React, { useEffect, useState } from 'react';
 import { AiOutlineThunderbolt } from 'react-icons/ai';
 import { useParams } from 'react-router-dom';
 
@@ -7,7 +8,12 @@ import Layout from '@/components/Layout';
 import ReviewButton from '@/components/detail/ReviewButton';
 import Reviewee from '@/components/detail/Reviewee';
 import TimelineItem from '@/components/detail/TimelineItem';
-import { pullRequest } from '@/data/dummyPullRequest';
+import { PullRequest } from '@/types/PullRequestType';
+
+// TODO: octokit の宣言を抽象化する
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_TOKEN,
+});
 
 type Path = {
   owner: string;
@@ -15,10 +21,37 @@ type Path = {
   pullNumber: string;
 };
 
+const initialPullRequest: PullRequest = {
+  title: '',
+  userName: '',
+  avatarUrl: '',
+  comment: '',
+};
+
 const DetailPage = () => {
   // TODO: eslint の例外設定を削除
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { owner, repo, pullNumber } = useParams<Path>();
+  const [pullRequest, setPullRequest] = useState<PullRequest>(initialPullRequest);
+
+  useEffect(() => {
+    octokit
+      .request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+        owner: owner,
+        repo: repo,
+        pull_number: Number(pullNumber), // NOTE: useParams の型定義は string でなければならない
+      })
+      .then((res) => {
+        console.log(res.data);
+        const data: PullRequest = {
+          title: res.data.title,
+          userName: res.data.assignee?.login,
+          avatarUrl: res.data.assignee?.avatar_url,
+          comment: res.data.body,
+        };
+        setPullRequest(data);
+      });
+  }, []);
 
   return (
     <Layout
