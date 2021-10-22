1617451d79dc1efc52c132bf491e307c2b3cbf95
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-22 11:43:17 +0900

    refactor: コードをより簡潔に変更

--- src/components/review/Popover.tsx
+++ src/components/review/Popover.tsx
@@ -14,33 +14,31 @@ import {
 
 const ButtonTextList = ['❓質問', '✨素敵', '🤔改善'];
 
-const ReviewPopover = () => {
-  return (
-    // TODO: Boxは作業用に見やすくするための出力場所移動用 後に削除
-    <Box p={300}>
-      <Popover>
-        <PopoverTrigger>
-          <Button>＋</Button>
-        </PopoverTrigger>
-        <Portal>
-          <PopoverContent>
-            <PopoverArrow />
-            <PopoverHeader>テンプレートを選んでみよう！</PopoverHeader>
-            <PopoverCloseButton />
-            <PopoverBody>
-              <VStack>
-                {ButtonTextList.map((ButtonText, index) => (
-                  <Button key={index} w="100%" colorScheme="teal">
-                    {ButtonText}
-                  </Button>
-                ))}
-              </VStack>
-            </PopoverBody>
-          </PopoverContent>
-        </Portal>
-      </Popover>
-    </Box>
-  );
-};
+const ReviewPopover = () => (
+  // TODO: Boxは作業用に見やすくするための出力場所移動用 後に削除
+  <Box p={300}>
+    <Popover>
+      <PopoverTrigger>
+        <Button>＋</Button>
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent>
+          <PopoverArrow />
+          <PopoverHeader>テンプレートを選んでみよう！</PopoverHeader>
+          <PopoverCloseButton />
+          <PopoverBody>
+            <VStack>
+              {ButtonTextList.map((ButtonText, index) => (
+                <Button key={index} w="100%" colorScheme="teal">
+                  {ButtonText}
+                </Button>
+              ))}
+            </VStack>
+          </PopoverBody>
+        </PopoverContent>
+      </Portal>
+    </Popover>
+  </Box>
+);
 
 export default ReviewPopover;
