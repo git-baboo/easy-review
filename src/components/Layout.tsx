6625de26c7edde28b81e64e64da51547f55ed2fb
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-23 16:41:38 +0900

    style: メニューボタンの配置の場所の変更

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Text, Icon, Flex, HStack } from '@chakra-ui/react';
+import { Text, Icon, Flex, HStack, Box } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
 import { IconType } from 'react-icons';
 
@@ -13,9 +13,9 @@ type Props = {
 const Layout = ({ text, icon, children }: Props) => {
   return (
     <>
-      <Flex alignItems="center" pl={16} h={36} bg="teal.500">
-        <HStack w="100%" mr={16} justifyContent="space-between">
-          <HStack alignItems="start">
+      <Flex alignItems="center" pl={20} pr={3} h={36} bg="teal.500">
+        <HStack w="100%" h="100%">
+          <HStack w="90%" alignItems="start">
             <Icon mt={1} as={icon} boxSize={6} color="teal.600" />
             <Text
               fontSize="lg"
@@ -27,7 +27,9 @@ const Layout = ({ text, icon, children }: Props) => {
               {text}
             </Text>
           </HStack>
-          <LogoutButton />
+          <Box h="100%" w="10%" align="right">
+            <LogoutButton />
+          </Box>
         </HStack>
       </Flex>
       {children}
