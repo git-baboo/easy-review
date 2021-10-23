9af1f4ef2347e0ecf90ba5be108b72da559ce658
Wataru Ono <wtr0414@gmail.com>
2021-10-23 14:21:30 +0900

    revert: ボタンを削除
    動作確認が完了したから

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
-import { Button, Container } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import React from 'react';
 import { BsCheckCircleFill } from 'react-icons/bs';
 
 import Layout from '@/components/Layout';
 import PullRequestList from '@/components/toppage/PullRequestList';
-import { useAuth } from '@/hooks/useAuth';
 
 const TopPage = () => {
-  const { logout } = useAuth();
-
   return (
     <Layout
       text={`あなた宛のレビューの依頼が届いているよ👀\nレビューするプルリクエストを選んでみよう！`}
@@ -16,8 +13,6 @@ const TopPage = () => {
     >
       <Container maxW="container.sm">
         <PullRequestList />
-        {/* TODO: 動作確認用。確認出来次第削除 */}
-        <Button onClick={logout}>logout</Button>
       </Container>
     </Layout>
   );
