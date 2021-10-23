3772dabb07275dfb8496815348394232cb7db080
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-23 19:56:05 +0000

    refactor: コメント表示部分のコンポーネントを分割
    1つのファイル内で入れ子になっていたため

--- src/components/review/Widget.tsx
+++ src/components/review/Widget.tsx
@@ -1,31 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Avatar } from '@chakra-ui/avatar';
 import { Button } from '@chakra-ui/button';
-import { Box, List, ListItem, Spacer, Text } from '@chakra-ui/layout';
+import { Box, List, Spacer } from '@chakra-ui/layout';
 import { Flex } from '@chakra-ui/react';
 import { Textarea } from '@chakra-ui/textarea';
 import React, { useCallback } from 'react';
 
-type Props = {
-  id: string;
-  author: string;
-  avatarUrl: string;
-  body: string;
-};
+import CommentList from '@/components/review/CommentList';
 
 const Widget = ({ changeKey, comments, draft, onDraftChange, onSubmit }: any) => {
-  const renderComment = ({ id, author, avatarUrl, body }: Props) => (
-    <ListItem key={id} m={2} p={4} border="1px" borderRadius="md" borderColor="gray.300">
-      <Flex alignItems="center">
-        <Avatar size="sm" name={author} src={avatarUrl} />
-        <Text ml={2} fontSize="md">
-          {author}
-        </Text>
-      </Flex>
-      <Text ml={10}>{body}</Text>
-    </ListItem>
-  );
-
   const input = useCallback(
     (e) => onDraftChange(changeKey, e.target.value),
     [onDraftChange, changeKey]
@@ -35,7 +17,7 @@ const Widget = ({ changeKey, comments, draft, onDraftChange, onSubmit }: any) =>
 
   return (
     <Box m={4}>
-      <List>{comments.map(renderComment)}</List>
+      <List>{comments.map(CommentList)}</List>
       <Textarea value={draft} onChange={input} />
       <Flex>
         <Spacer />
