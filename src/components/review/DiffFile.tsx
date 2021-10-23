22fbfa04f502a8cd8ae8d2fa2333805b317f83f2
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-23 23:38:16 +0000

    feat: 直書きのポップオーバーを外部ファイルから読み込むように変更

--- src/components/review/DiffFile.tsx
+++ src/components/review/DiffFile.tsx
@@ -1,20 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Button } from '@chakra-ui/button';
 import { PlusSquareIcon } from '@chakra-ui/icons';
-import { Box, Heading, VStack } from '@chakra-ui/layout';
-import {
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverHeader,
-  PopoverTrigger,
-} from '@chakra-ui/popover';
-import { Portal } from '@chakra-ui/portal';
+import { Box, Heading } from '@chakra-ui/layout';
 import { useMemo } from 'react';
 
 import '@/style/difffile.css';
+import ReviewPopover from '@/components/review/Popover';
 
 const reactDiffView = require('react-diff-view');
 const Diff = reactDiffView.Diff;
@@ -33,31 +23,12 @@ type Props = {
 
 const DiffFile = ({ oldPath, newPath, type, hunks, widgets, addWidget }: Props) => {
   const headerPath = oldPath === newPath ? oldPath : `${oldPath} → ${newPath}`;
-  const ButtonTextList = ['❓ 質問', '✨ 素敵', '🤔 改善'];
 
   const renderGutter = ({ side, renderDefault, inHoverState }: any) =>
     inHoverState && side === 'new' ? (
-      <Popover>
-        <PopoverTrigger>
-          <PlusSquareIcon color="white" bgColor="blue.500" />
-        </PopoverTrigger>
-        <Portal>
-          <PopoverContent>
-            <PopoverArrow />
-            <PopoverHeader>テンプレートを選んでみよう！</PopoverHeader>
-            <PopoverCloseButton />
-            <PopoverBody>
-              <VStack>
-                {ButtonTextList.map((ButtonText, index) => (
-                  <Button key={index} w="100%" colorScheme="teal">
-                    {ButtonText}
-                  </Button>
-                ))}
-              </VStack>
-            </PopoverBody>
-          </PopoverContent>
-        </Portal>
-      </Popover>
+      <ReviewPopover>
+        <PlusSquareIcon color="white" bgColor="blue.500" />
+      </ReviewPopover>
     ) : (
       renderDefault()
     );
