e003efdcb48efe35fd8df661e542ef06fec1d2eb
Wataru Ono <wtr0414@gmail.com>
2021-10-23 14:15:43 +0900

    feat: 動作確認用のログアウトボタンの追加
    動作確認時にログアウトしたいから

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,26 @@
-import { Container } from '@chakra-ui/react';
+import { Button, Container } from '@chakra-ui/react';
 import React from 'react';
 import { BsCheckCircleFill } from 'react-icons/bs';
 
 import Layout from '@/components/Layout';
 import PullRequestList from '@/components/toppage/PullRequestList';
+import { useAuth } from '@/hooks/useAuth';
 
-const TopPage = () => (
-  <Layout
-    text={`あなた宛のレビューの依頼が届いているよ👀\nレビューするプルリクエストを選んでみよう！`}
-    icon={BsCheckCircleFill}
-  >
-    <Container maxW="container.sm">
-      <PullRequestList />
-    </Container>
-  </Layout>
-);
+const TopPage = () => {
+  const { logout } = useAuth();
+
+  return (
+    <Layout
+      text={`あなた宛のレビューの依頼が届いているよ👀\nレビューするプルリクエストを選んでみよう！`}
+      icon={BsCheckCircleFill}
+    >
+      <Container maxW="container.sm">
+        <PullRequestList />
+        {/* TODO: 動作確認用。確認出来次第削除 */}
+        <Button onClick={logout}>logout</Button>
+      </Container>
+    </Layout>
+  );
+};
 
 export default TopPage;
