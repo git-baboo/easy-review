9986ad2adeb42041075be7b86e9a412f8e94f474
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-23 15:21:23 +0900

    style: メニューボタンの配置

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,14 +1,20 @@
-import { Button } from '@chakra-ui/react';
+import { AddIcon, EmailIcon, HamburgerIcon } from '@chakra-ui/icons';
+import { Menu, MenuList, MenuItem, MenuButton, IconButton } from '@chakra-ui/react';
 import React from 'react';
 
-import { useAuth } from '@/hooks/useAuth';
+// import { useAuth } from '@/hooks/useAuth';
 
 const LogoutButton = () => {
-  const { logout } = useAuth();
+  // const { logout } = useAuth();
+
   return (
-    <Button bgColor="white" color="teal.500" onClick={logout}>
-      ログアウト
-    </Button>
+    <Menu>
+      <MenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon />} variant="outline" />
+      <MenuList>
+        <MenuItem icon={<AddIcon />}>ログアウト</MenuItem>
+        <MenuItem icon={<EmailIcon />}>お問い合わせ</MenuItem>
+      </MenuList>
+    </Menu>
   );
 };
 
