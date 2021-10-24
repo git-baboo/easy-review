25ab3a45a3fa3d31195cb50a05646ba8122f1b78
Wataru Ono <wtr0414@gmail.com>
2021-10-24 11:14:18 +0900

    feat: リポジトリへのアクセス権限の要求
    プルリクにコメントを投稿するため

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -11,6 +11,7 @@ export const useAuth = () => {
   const history = useHistory();
   const setCurrentUser = useSetRecoilState<CurrentUserType>(currentUserState);
   const provider = new GithubAuthProvider();
+  provider.addScope('repo');
 
   const login = () => {
     signInWithPopup(auth, provider).then((result) => {
