8e919b40d6f6c0931c506e3059134d05372ea838
Wataru Ono <wtr0414@gmail.com>
2021-11-25 20:44:41 +0900

    feat: ESLint と Prettier の導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
+import { NextPage } from "next";
+import type { AppProps } from "next/app";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
