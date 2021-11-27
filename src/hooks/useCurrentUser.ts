51798273c19a21eb8470f769e93bc5e3ecbd5b34
Wataru Ono <wtr0414@gmail.com>
2021-11-27 19:55:53 +0900

    feat: トップページの作成

---
+++ src/hooks/useCurrentUser.ts
@@ -0,0 +1,15 @@
+import { useRecoilValue } from "recoil";
+
+import { currentUserState } from "@/store/currentUserState";
+import { CurrentUserType } from "@/types/CurrentUserType";
+
+export const useCurrentUser = () => {
+  const currentUser = useRecoilValue<CurrentUserType>(currentUserState);
+
+  const isSignedIn = currentUser.isSignedIn;
+  const username = currentUser.username;
+  const accessToken = currentUser.accessToken;
+  const reviewId = currentUser.reviewId;
+
+  return { isSignedIn, username, accessToken, reviewId };
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </ChakraProvider>
   );
 };
