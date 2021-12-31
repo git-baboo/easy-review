3120bf6ef232d56f96d4e38f150548713feb8209
Morisaki Mikiya <git@morisakimikiya.com>
2021-12-31 04:15:25 +0000

    feat: バイナリファイルの解析処理を追加
    ライブラリ側 (parseDiff()関数) で情報を正しく取得できないため

--- src/components/review/DiffFileList.tsx
+++ src/components/review/DiffFileList.tsx
@@ -20,7 +20,53 @@ type CustomProps = {
 type Props = StackProps & CustomProps;
 
 const DiffFileList = ({ diff, widgets, addWidget, ...props }: Props) => {
+  const parseBinary = (
+    diff: string
+  ): {
+    isBinary?: boolean;
+    type?: string;
+    oldPath?: string;
+    newPath?: string;
+  }[] => {
+    // trim(): 末尾の改行削除。2行下で lastLine を正しく取得するため
+    // "diff --git ..." の行がファイル境界
+    const files = diff.trim().split("\ndiff --git");
+    return files.map((file) => {
+      const lastLine = file.split("\n").slice(-1)[0];
+      const words = lastLine.split(" ");
+      const info: {
+        isBinary?: boolean;
+        type?: string;
+        oldPath?: string;
+        newPath?: string;
+      } = {};
+      if (words[0] === "Binary") {
+        info.isBinary = true;
+        const oldPath = words[2];
+        const newPath = words[4];
+        if (oldPath === "/dev/null") {
+          info.type = "add";
+          info.oldPath = oldPath;
+          info.newPath = newPath.slice(2);
+        } else if (newPath === "/dev/null") {
+          info.type = "delete";
+          info.oldPath = oldPath.slice(2);
+          info.newPath = newPath;
+        } else {
+          info.type = "modify";
+          info.oldPath = oldPath.slice(2);
+          info.newPath = newPath.slice(2);
+        }
+      }
+      return info;
+    });
+  };
+
   const files = parseDiff(diff);
+  const binaryFiles = parseBinary(diff);
+  for (let i = 0; i < files.length; i++) {
+    files[i] = { ...files[i], ...binaryFiles[i] };
+  }
 
   return (
     <VStack {...props}>
