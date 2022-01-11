b90e77cd86d891fa10eccffa95e7cd3f1078586e
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-01-11 14:37:42 +0900

    feat: 共通の PR 情報を表示するファイルの作成
    分れているファイルを共通化するため

---
+++ src/components/PullRequestInfomation.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/layout";
+import React from "react";
+
+const PullRequestInformation = () => {
+  return (
+    <Text fontSize="xs" color="gray.600">
+      a
+    </Text>
+  );
+};
+
+export default PullRequestInformation;
