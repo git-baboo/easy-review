f246db3b13cb2bf38128894b65ac82663cda8a91
Morisaki Mikiya <git@morisakimikiya.com>
2022-01-13 01:07:34 +0000

    feat: プルリクがないときのメッセージを作成
    なにも表示されない状態を解消するため

---
+++ src/components/top/NoPullsMessage.tsx
@@ -0,0 +1,31 @@
+import { ArrowForwardIcon, WarningIcon } from "@chakra-ui/icons";
+import { Box, Center, HStack, Link, Text } from "@chakra-ui/react";
+
+const NoPullsMessage = () => {
+  return (
+    <Box mt={12}>
+      <Center>
+        <HStack px="36px" py="26px" spacing="16px">
+          <WarningIcon w="32px" h="32px" color="gray.300" />
+          <Text fontSize="sm" lineHeight={5} fontWeight="bold">
+            あなたがレビュワーに指定されている
+            <br />
+            プルリクエストが見つかりませんでした
+          </Text>
+        </HStack>
+      </Center>
+      <HStack mt={2} justify="end">
+        <Link href="#" color="gray.600">
+          <HStack spacing={1}>
+            <Text fontSize="xs" lineHeight={4}>
+              正しく表示されないときは
+            </Text>
+            <ArrowForwardIcon w="11px" h="11px" />
+          </HStack>
+        </Link>
+      </HStack>
+    </Box>
+  );
+};
+
+export default NoPullsMessage;
