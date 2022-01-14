6d543c80b9fddb2171fb0e173894f04d1b950a8f
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-01-14 10:54:05 +0900

    feat: ヘッダーの作成

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,9 +1,20 @@
+import { Container, Text } from "@chakra-ui/layout";
 import React from "react";
+import { AiFillQuestionCircle } from "react-icons/ai";
 
+import Layout from "@/components/Layout";
 import withAuth from "@/hoc/withAuth";
 
 export const Faq = () => {
-  return <p>FAQ</p>;
+  return (
+    <Layout text={"よくある質問"} icon={AiFillQuestionCircle}>
+      <Container py={9} maxW="container.sm">
+        <Text fontSize="xs" color="gray.600">
+          FAQ
+        </Text>
+      </Container>
+    </Layout>
+  );
 };
 
 export default withAuth(Faq);
