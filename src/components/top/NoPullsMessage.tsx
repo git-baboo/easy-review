29615e39c026b9d22bb0e086441be671e70af514
Morisaki Mikiya <git@morisakimikiya.com>
2022-01-24 09:30:37 +0000

    fix: メッセージボックスの横幅を修正
    container.sm → アイコンとメッセージから px={9} になるようにするため

--- src/components/top/NoPullsMessage.tsx
+++ src/components/top/NoPullsMessage.tsx
@@ -3,9 +3,9 @@ import { Box, Center, HStack, Link, Text } from "@chakra-ui/react";
 
 const NoPullsMessage = () => {
   return (
-    <Box mt={12}>
-      <Center boxShadow="xs">
-        <HStack px={9} py={6} spacing={4}>
+    <Center mt={12}>
+      <Box>
+        <HStack px={9} py={6} spacing={4} boxShadow="xs">
           <WarningIcon w={8} h={8} color="gray.300" />
           <Text fontSize="sm" lineHeight={5} fontWeight="bold">
             あなたがレビュワーに指定されている
@@ -13,15 +13,15 @@ const NoPullsMessage = () => {
             プルリクエストが見つかりませんでした
           </Text>
         </HStack>
-      </Center>
-      <HStack mt={2} justify="end">
-        <Link href="#" color="gray.600">
-          <Text fontSize="xs" lineHeight={4}>
-            正しく表示されないときは →
-          </Text>
-        </Link>
-      </HStack>
-    </Box>
+        <HStack mt={2} justify="end">
+          <Link href="#" color="gray.600">
+            <Text fontSize="xs" lineHeight={4}>
+              正しく表示されないときは →
+            </Text>
+          </Link>
+        </HStack>
+      </Box>
+    </Center>
   );
 };
 
