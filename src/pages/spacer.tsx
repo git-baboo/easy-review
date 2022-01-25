03b2ca1e07042112b41f6b96fb5b0b0785e9153d
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-01-25 13:08:38 +0900

    feat: Spacerコンポーネントの大枠の作成

---
+++ src/pages/spacer.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+type Props = {
+  horizontal: boolean;
+  heightSize: number;
+  widthSize: number;
+};
+
+export const Spacer = ({ horizontal, heightSize, widthSize }: Props) => {
+  return horizontal ? (
+    <Box bg="black" w={"100%"} p={heightSize}></Box>
+  ) : (
+    <Box bg="tomato" w={widthSize} p={heightSize}></Box>
+  );
+};
+
+export default Spacer;
