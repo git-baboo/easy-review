e3ad83c627fbf95c2ccd4d8f3baaab6d67b87710
Wataru Ono <wtr0414@gmail.com>
2022-02-05 01:23:21 +0900

    refactor: フォーマット
    Prettier に従うため

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,10 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1.0"
+          />
           <meta
             name="description"
             content="Easy Reviewは初心者エンジニアのコードレビューに対する不安感や障壁を払拭します。"
