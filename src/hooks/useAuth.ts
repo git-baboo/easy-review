0c2b7e96669fec08ae74916ad9f49bfc844d7e1c
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-02-08 09:59:13 +0900

    feat: signInWithRedirect を使ったログインの実装
    signInWithPopup から signInWithRedirect に移行するため

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,4 @@
-import { Octokit } from "@octokit/rest";
-import { GithubAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, signInWithRedirect, signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 import { useSetRecoilState } from "recoil";
 
@@ -14,31 +13,32 @@ export const useAuth = () => {
   provider.addScope("repo");
 
   const login = () => {
-    signInWithPopup(auth, provider).then((result) => {
-      const credential = GithubAuthProvider.credentialFromResult(result);
-      if (credential) {
-        const token = credential.accessToken;
+    signInWithRedirect(auth, provider);
+    // signInWithPopup(auth, provider).then((result) => {
+    //   const credential = GithubAuthProvider.credentialFromResult(result);
+    //   if (credential) {
+    //     const token = credential.accessToken;
 
-        const octokit = new Octokit({
-          auth: token,
-        });
+    //     const octokit = new Octokit({
+    //       auth: token,
+    //     });
 
-        octokit.request("GET /user").then((res) => {
-          setCurrentUser((prevState) => ({
-            ...prevState,
-            username: res.data.login,
-          }));
-        });
+    //     octokit.request("GET /user").then((res) => {
+    //       setCurrentUser((prevState) => ({
+    //         ...prevState,
+    //         username: res.data.login,
+    //       }));
+    //     });
 
-        setCurrentUser((prevState) => ({
-          ...prevState,
-          isSignedIn: true,
-          accessToken: String(token),
-        }));
+    //     setCurrentUser((prevState) => ({
+    //       ...prevState,
+    //       isSignedIn: true,
+    //       accessToken: String(token),
+    //     }));
 
-        router.push("/");
-      }
-    });
+    //     router.push("/");
+    //   }
+    // });
   };
 
   const logout = () => {
