6f46c2f528827972a5b4b8bafc2b021ab9044ca8
Wataru Ono <wtr0414@gmail.com>
2022-02-11 17:14:40 +0900

    Revert "feat: メソッドの単体テストを実行できる環境の構築"
    This reverts commit 9d8f204fb081e4f25bdd4b0bb27a9a84514120fa.

--- src/tests/utils/sample/sample.test.ts
+++
@@ -1,7 +0,0 @@
-import { sample } from "@/tests/utils/sample/sample";
-
-describe("sample", () => {
-  test("1が返ってくる", () => {
-    expect(sample()).toBe(1);
-  });
-});
--- src/tests/utils/sample/sample.ts
+++
@@ -1,3 +0,0 @@
-export const sample = (): number => {
-  return 1;
-};
