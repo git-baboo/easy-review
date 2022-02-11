bbe0a837a2117cee7c04c653e47aa8fb2640559a
Wataru Ono <wtr0414@gmail.com>
2022-02-11 17:13:17 +0900

    Revert "feat: signInWithRedirect を使ったログインの実装"
    This reverts commit 1e48955070010ee27e69e97c715714ef39d86fcd.

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,4 +1,5 @@
-import { GithubAuthProvider, signInWithRedirect, signOut } from "firebase/auth";
+import { Octokit } from "@octokit/rest";
+import { GithubAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 import { useSetRecoilState } from "recoil";
 
@@ -13,32 +14,31 @@ export const useAuth = () => {
   provider.addScope("repo");
 
   const login = () => {
-    signInWithRedirect(auth, provider);
-    // signInWithPopup(auth, provider).then((result) => {
-    //   const credential = GithubAuthProvider.credentialFromResult(result);
-    //   if (credential) {
-    //     const token = credential.accessToken;
+    signInWithPopup(auth, provider).then((result) => {
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      if (credential) {
+        const token = credential.accessToken;
 
-    //     const octokit = new Octokit({
-    //       auth: token,
-    //     });
+        const octokit = new Octokit({
+          auth: token,
+        });
 
-    //     octokit.request("GET /user").then((res) => {
-    //       setCurrentUser((prevState) => ({
-    //         ...prevState,
-    //         username: res.data.login,
-    //       }));
-    //     });
+        octokit.request("GET /user").then((res) => {
+          setCurrentUser((prevState) => ({
+            ...prevState,
+            username: res.data.login,
+          }));
+        });
 
-    //     setCurrentUser((prevState) => ({
-    //       ...prevState,
-    //       isSignedIn: true,
-    //       accessToken: String(token),
-    //     }));
+        setCurrentUser((prevState) => ({
+          ...prevState,
+          isSignedIn: true,
+          accessToken: String(token),
+        }));
 
-    //     router.push("/");
-    //   }
-    // });
+        router.push("/");
+      }
+    });
   };
 
   const logout = () => {
