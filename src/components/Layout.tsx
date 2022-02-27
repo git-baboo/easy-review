4dd01b48f153add624dec7488a8f36c0c2831289
Wataru Ono <wtr0414@gmail.com>
2022-02-27 17:06:06 +0900

    refactor: Menuコンポーネントの削除
    Menu部分をMenuコンポーネントとして切り出すのは設計としてあまり良くないと感じたため

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,20 @@
-import { Text, Icon, Flex, Box, Spacer } from "@chakra-ui/react";
+import { ArrowBackIcon, EmailIcon, HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Text,
+  Icon,
+  Flex,
+  Spacer,
+  Menu,
+  MenuButton,
+  IconButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
 import React, { ReactNode } from "react";
 import { IconType } from "react-icons";
 
-import Menu from "@/components/Menu";
 import { HSpacer } from "@/components/Spacer";
+import { useAuth } from "@/hooks/useAuth";
 
 type Props = {
   text: string;
@@ -12,6 +23,8 @@ type Props = {
 };
 
 const Layout = ({ text, icon, children }: Props) => {
+  const { logout } = useAuth();
+
   return (
     <>
       <Flex bgColor="teal.500" h={36}>
@@ -31,9 +44,28 @@ const Layout = ({ text, icon, children }: Props) => {
 
         <Spacer />
 
-        <Box>
-          <Menu />
-        </Box>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            icon={<HamburgerIcon w={6} h={6} />}
+            color="white"
+            colorScheme="ghost"
+            _focus={{ boxShadow: "none" }}
+          />
+          <MenuList>
+            <MenuItem icon={<ArrowBackIcon />} onClick={logout}>
+              ログアウト
+            </MenuItem>
+            <MenuItem
+              icon={<EmailIcon />}
+              onClick={() => {
+                window.open("https://forms.gle/W4s7xqEiAeskEof38");
+              }}
+            >
+              フィードバック
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
       {children}
     </>
